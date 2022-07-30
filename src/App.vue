787615e376a7ad81b4683c7b2bd9bf47c00d4c0f
Mihail <mohonovproduction@yandex.ru>
2022-07-30 14:35:23 +0300

    auth views, styles, methods

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,25 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
+
 :root {
   padding: 0;
   margin: 0;
   box-sizing: content-box;
   --bg: hsla(0, 0%, 7%, 1);
+  --bg-gray: hsla(0, 0%, 47%, 1);
+  --bg-white: hsla(0, 0%, 96%, 1);
   --font-white: hsla(0, 0%, 100%, 1);
-  font-size: calc(.5em + 1vw);
+  --primary-green: hsla(141, 76%, 48%, 1);
+  font-size: calc(.8em + 1vw);
+  font-family: 'Open Sans', sans-serif;
+  letter-spacing: .02em;
+  word-spacing: .04em;
+  line-height: 1.3em;
   color: var(--font-white);
   background-color: var(--bg);
-
+  min-height: 100vh;
 }
 
 *,
@@ -21,5 +30,35 @@
   padding: 0;
   margin: 0;
   box-sizing: inherit;
+}
+
+.primary-line {
+  background-color: var(--primary-green);
+  color: var(--bg);
+  font-style: italic;
+  padding: 0 .25em;
+}
+
+textarea {
+  font-size: 1em;
+  color: var(--font-white);
+  padding: .5em;
+  background-color: var(--bg-gray);
+  border: none;
+  border-radius: .4em;
+  outline: none;
+}
+textarea:active {
+  outline: var(--primary-green) solid .2em;
+}
+
+.button-gray {
+  color: var(--bg);
+  font-weight: 700;
+  padding: 1em 2.5em;
+  background-color: var(--bg-white);
+  border: none;
+  border-radius: 3em;
+  width: min-content;
 }
 </style>
--- src/views/Authorization.vue
+++ src/views/Authorization.vue
@@ -1,24 +1,56 @@
 <template>
-  <article class="form">
-    <label class="form__label">Please enter your token</label>
-    <textarea class="form__textarea"></textarea>
-    <button class="button-gray">Authorization</button>
-  </article>
+  <section class="wrapper">
+    <h1 class="heading">Authorization</h1>
+    <article class="form">
+      <label class="form__label">Please enter your <span class="primary-line">token</span></label>
+      <textarea @keydown.enter="authorization" v-model="token" class="form__textarea" rows="1"></textarea>
+      <button @click="authorization" class="button-gray">Authorization</button>
+    </article>
+  </section>
 </template>
 
 <script>
+import router from "@/router";
+
 export default {
-  name: "Authorization"
+  name: "Authorization",
+  data() {
+    return {
+      token: null,
+    }
+  },
+  methods: {
+    authorization() {
+      router.push('/')
+    }
+  }
 }
 </script>
 
 <style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.heading {
+  text-align: center;
+  margin-top: 2em;
+  padding: 1em;
+}
 .form {
   display: grid;
-  grid-gap: 2em;
-  padding: 2rem;
+  grid-gap: .5em;
+  max-width: 640px;
+  min-width: 260px;
+  font-weight: 600;
+  padding: 0 2.5rem;
 }
 .form__label {
-
+  font-weight: 400;
+}
+.button-gray {
+  margin-top: 1em;
+  justify-self: end;
 }
 </style>
